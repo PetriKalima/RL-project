3f64fb86dd434a4ef0eeec29a052752c2c9c9b7a
Petri Kalima <petri.kalima@citrus.fi>
2019-11-18 17:29:43 +0200

    DQN

--- wimblepong/agent.py
+++ wimblepong/agent.py
@@ -1,10 +1,31 @@
-import torch
 import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
 
 
+class DQN(nn.Module):
+    def __init__(self, state_space_dim, action_space_dim, hidden=12):
+        super(DQN, self).__init__()
+        self.hidden = hidden
+        self.conv1 = nn.Conv2d(state_space_dim, 32, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+        #self.fc1 = nn.Linear(state_space_dim, hidden)
+        #self.fc2 = nn.Linear(hidden, action_space_dim)
 
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.conv3(x)
+        x = F.relu(x)
+        return x
 
-class Agent():
+class Agent(object):
 
-    def __init__():
-        return
+    def __init__(self, state_space, n_actions, replay_buffer_size=50000,
+                 batch_size=32, hidden_size=12, gamma=0.98):
+        self.n_actions = n_actions
