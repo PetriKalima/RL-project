80c8a1c0d37d5782285837451190b6289da0bc7e
Petri Kalima <petri.kalima@citrus.fi>
2019-12-06 17:22:29 +0200

    correct test pong ai instead

--- test_pong_ai.py
+++ test_pong_ai.py
@@ -9,7 +9,7 @@ import gym
 import numpy as np
 import argparse
 import wimblepong
-from wimblepong.agent import Agent
+from wimblepong.agenthit import Agent
 import torch
 from time import sleep
 
@@ -32,11 +32,12 @@ env.unwrapped.scale = args.scale
 env.unwrapped.fps = args.fps
 
 # Number of episodes/games to play
-#episodes = 100000
 episodes = 1000000
-TARGET_UPDATE_FREQ=1000  #hua
-LEARNING_STARTS = 59000
-#LEARNING_STARTS = 2000
+#episodes = 1000
+
+TARGET_UPDATE_FREQ=1000 
+LEARNING_STARTS = 50000
+#LEARNING_STARTS = 2000  # for testing fast
 
 # Define the player
 player_id = 1
@@ -44,42 +45,66 @@ player_id = 1
 #player = wimblepong.SimpleAi(env, player_id)
 player = Agent(env, env.observation_space.shape[0], player_id)
 
+# Training loop
+cumulative_rewards = []
+#  for Priority Memory
+statesP=[]  # for prio
+actionsP=[]
+ob1sP=[] 
+rew1sP=[]
+donesP=[]
+flagwin_epoNotadded= True  # for prio
+
 # Housekeeping
 states = []
 rewards = []
 win1 = 0
 step = 0
-ballhitreward = 5
+ballhitreward = 1
 curPlayer = env.player1
+
 
 if args.model:
     player.load_model(args.model)
 for i in range(0,episodes):
     done = False
     player.reset()
-    #player.epsilon = 200/(i+200)
+    ball_pre_x=env.ball.x
+    cum_reward = 0
+
+    state=player.state # for Prio
+
     while not done:
-        # action1 is zero because in this example no agent is playing as player 0
-        #action1 = 0
         step += 1
         if step % 1000 == 0:
             print(step)
         if not args.model:    
-            player.epsilon = max(0.1, 1 - step / 60000)
+            player.epsilon = max(0.1, 1 - step / 1000000)
+            #player.epsilon = max(0.1, 1 - step / 5000)  # for short testing
         if step == 1 or step % 2 == 0:
             action1 = player.get_action()
         ob1, rew1, done, info = env.step(action1)
+        cum_reward += rew1 
+        
+        #for Priority memory start
+        statesP.append(state)        
+        actionsP.append(action1)
+        ob1sP.append(ob1) 
+        rew1sP.append(rew1)
+        donesP.append(done)
+        # for priority memory end
+
         if rew1 != 10 and rew1 != -10:
-            #print(env.ball.y, env.player1.y)
-            #sleep(1)
-            #print(env.ball.x, curPlayer.x)
-            #if curPlayer == env.player2:
-            #    sleep(1)
             if env.ball.y < curPlayer.y + 14 and env.ball.y > curPlayer.y - 14 and abs(env.ball.x - curPlayer.x) < 10:
-                rew1 = ballhitreward 
-                #print(rew1)
-        player.store_transition(player.state, action1, ob1, rew1, done)
+                if curPlayer.x <=10 and (env.ball.x>= ball_pre_x):  # add 0612
+                   rew1 = ballhitreward 
+                if curPlayer.x >=120 and (env.ball.x<= ball_pre_x):   
+                   rew1 = ballhitreward
+        #player.store_transition(player.state, action1, ob1, rew1, done)  #  mod for Priority memory
         player.state = ob1
+        state=ob1  # for priority memory
+        ball_pre_x=env.ball.x #add 0612
+
         rewards.append(rew1)
         if args.housekeeping:
             states.append(ob1)
@@ -109,12 +134,66 @@ for i in range(0,episodes):
         if step < LEARNING_STARTS:
             continue
         if step % TARGET_UPDATE_FREQ == 0:
-            player.replace_targetpolicy()            
-        if step % 4 == 0:
-            player.update_network()
-    #if i % 4 == 0:
-    #    player.update_network()            
-        #player.target_net.load_state_dict(player.policy_net.state_dict())
+            player.replace_targetpolicy()             
+        player.update_network() 
+
+    cumulative_rewards.append(cum_reward) 
+
+    # for priority memory
+
+
+    print("rew1, cum_reward:", rew1,cum_reward)
+    
+    if player.memory.__len__()<1000:
+        if (rew1 ==-10 and cum_reward>-10) or (rew1 ==10 and  cum_reward>10) : 
+           for k in range(len(statesP)):
+              player.store_transition(statesP[k], actionsP[k], ob1sP[k], rew1sP[k], donesP[k])
+    else:   
+    
+        if done and rew1 ==10 and  cum_reward>10 and (flagwin_epoNotadded==True):
+           for k in range(len(statesP)):
+              player.store_transition(statesP[k], actionsP[k], ob1sP[k], rew1sP[k], donesP[k])
+           flagwin_epoNotadded = False
+
+        if done and rew1 ==-10 and cum_reward>-10 and (flagwin_epoNotadded==False ): 
+           for k in range(len(statesP)):
+              player.store_transition(statesP[k], actionsP[k], ob1sP[k], rew1sP[k], donesP[k])
+           flagwin_epoNotadded= True
+
+    statesP.clear()        
+    actionsP.clear() 
+    ob1sP.clear() 
+    rew1sP.clear() 
+    donesP.clear()         
+        
+
+
+
+    # for Priority memory save end
+      
+
+
+    if i % 300:
+
+        plt.figure(2)
+        plt.clf()
+        rewards_t = torch.tensor(cumulative_rewards, dtype=torch.float)
+        plt.title('Training...')
+        plt.xlabel('Episode')
+        plt.ylabel('Cumulative reward')
+        plt.grid(True)
+        plt.plot(rewards_t.numpy())
+        # Take 100 episode averages and plot them too
+        if len(rewards_t) >= 100:
+            means = rewards_t.unfold(0, 100, 1).mean(1).view(-1)
+            means = torch.cat((torch.zeros(99), means))
+            plt.plot(means.numpy())
+
+        plt.savefig("plot.png")
+        
     if i % 50 == 0:
         player.save_model()            
 
+print('Complete')
+plt.ioff()
+plt.show()
