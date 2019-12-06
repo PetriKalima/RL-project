455660af1bb3eeeefe4ce4ad1d7ef9fed6b00be5
Hua Huang <hua.huang@aalto.fi>
2019-12-06 09:26:56 +0200

    change hit rewards to 1, add code for avoid double count rewards.

---
+++ test_pong_aihit.py
@@ -0,0 +1,175 @@
+"""
+This is an example on how to use the two player Wimblepong environment with one
+agent and the SimpleAI
+"""
+import matplotlib.pyplot as plt
+from random import randint
+import pickle
+import gym
+import numpy as np
+import argparse
+import wimblepong
+from wimblepong.agenthit import Agent
+import torch
+from time import sleep
+#from utils import plot_rewards
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--headless", action="store_true", help="Run in headless mode")
+parser.add_argument("--housekeeping", action="store_true", help="Plot, player and ball positions and velocities at the end of each episode")
+parser.add_argument("--fps", type=int, help="FPS for rendering", default=30)
+parser.add_argument("--scale", type=int, help="Scale of the rendered game", default=1)
+parser.add_argument("-m", "--model", help="Model file to load")
+parser.add_argument("--cuda", default= True, action="store_true", help="Enable cuda")  #hua
+
+
+args = parser.parse_args()
+
+device = torch.device("cuda" if args.cuda else "cpu") #hua
+
+# Make the environment
+env = gym.make("WimblepongVisualSimpleAI-v0")
+env.unwrapped.scale = args.scale
+env.unwrapped.fps = args.fps
+
+# Number of episodes/games to play
+#episodes = 100000
+episodes = 1000000 # org 0512
+#episodes = 1000
+
+TARGET_UPDATE_FREQ=1000  #hua
+LEARNING_STARTS = 59000  #org 0512
+#LEARNING_STARTS = 2000  # for testing fast
+
+# Define the player
+player_id = 1
+# Set up the player here. We used the SimpleAI that does not take actions for now
+#player = wimblepong.SimpleAi(env, player_id)
+player = Agent(env, env.observation_space.shape[0], player_id)
+
+# Training loop
+cumulative_rewards = []
+#  for Priority Memory
+statesP=[]  # for prio
+actionsP=[]
+ob1sP=[] 
+rew1sP=[]
+donesP=[]
+flagwin_epoNotadded= True  # for prio
+
+# Housekeeping
+states = []
+rewards = []
+win1 = 0
+step = 0
+#ballhitreward = 5
+ballhitreward = 1
+curPlayer = env.player1
+
+
+if args.model:
+    player.load_model(args.model)
+for i in range(0,episodes):
+    done = False
+    player.reset()
+    ball_pre_x=env.ball.x  #add 0612
+    cum_reward = 0
+    #player.epsilon = 200/(i+200)
+    while not done:
+        # action1 is zero because in this example no agent is playing as player 0
+        #action1 = 0
+        step += 1
+        if step % 1000 == 0:
+            print(step)
+        if not args.model:    
+            player.epsilon = max(0.1, 1 - step / 1000000) # 0512 org
+            #player.epsilon = max(0.1, 1 - step / 5000)  # for short testing
+        if step == 1 or step % 2 == 0:
+            action1 = player.get_action()
+        ob1, rew1, done, info = env.step(action1)
+        cum_reward += rew1  #add 0512
+
+        if rew1 != 10 and rew1 != -10:
+            #print(env.ball.y, env.player1.y)
+            #sleep(1)
+            #print(env.ball.x, curPlayer.x)
+            #if curPlayer == env.player2:
+            #    sleep(1)
+            if env.ball.y < curPlayer.y + 14 and env.ball.y > curPlayer.y - 14 and abs(env.ball.x - curPlayer.x) < 10:
+                if curPlayer.x <=10 and (env.ball.x> ball_pre_x):  # add 0612
+                   rew1 = ballhitreward 
+                if curPlayer.x >=120 and (env.ball.x< ball_pre_x):   
+                   rew1 = ballhitreward
+                #print(rew1)
+        player.store_transition(player.state, action1, ob1, rew1, done)
+        player.state = ob1
+        ball_pre_x=env.ball.x #add 0612
+        rewards.append(rew1)
+        if args.housekeeping:
+            states.append(ob1)
+        # Count the wins
+        if rew1 == 10:
+            win1 += 1
+        if step % 500 == 0:
+            print("mean reward: ", np.mean(rewards[-500:]))    
+        if not args.headless:
+        #if step > 60000:
+            env.render()
+        if done:
+            observation= env.reset()
+            plt.close()  # Hides game window
+            if args.housekeeping:
+                plt.plot(states)
+                plt.legend(["Player", "Opponent", "Ball X", "Ball Y", "Ball vx", "Ball vy"])
+                plt.show()
+                states.clear()
+            print("episode {} over. Broken WR: {:.3f}".format(i, win1/(i+1)))
+            if i % 5 == 4:
+                env.switch_sides()
+                if curPlayer == env.player1:
+                    curPlayer = env.player2
+                else:
+                    curPlayer = env.player1    
+        if step < LEARNING_STARTS:
+            continue
+        if step % TARGET_UPDATE_FREQ == 0:
+            player.replace_targetpolicy()            
+        '''
+        if step % 4 == 0:  # original  0512
+        
+            player.update_network()
+        '''    
+        player.update_network() 
+
+    cumulative_rewards.append(cum_reward) 
+    #plot_rewards(cumulative_rewards)      
+    #if i % 4 == 0:
+    #    player.update_network()            
+        #player.target_net.load_state_dict(player.policy_net.state_dict())
+      
+    if i % 300:
+        #plt.plot(cumulative_rewards)
+
+        plt.figure(2)
+        plt.clf()
+        rewards_t = torch.tensor(cumulative_rewards, dtype=torch.float)
+        plt.title('Training...')
+        plt.xlabel('Episode')
+        plt.ylabel('Cumulative reward')
+        plt.grid(True)
+        plt.plot(rewards_t.numpy())
+        # Take 100 episode averages and plot them too
+        if len(rewards_t) >= 100:
+            means = rewards_t.unfold(0, 100, 1).mean(1).view(-1)
+            means = torch.cat((torch.zeros(99), means))
+            #means=means.cpu()
+            plt.plot(means.numpy())
+
+        plt.savefig("plot.png")
+        
+    if i % 50 == 0:
+        player.save_model()            
+
+print('Complete')
+plt.ioff()
+plt.show()
---
+++ wimblepong/agenthit.py
@@ -0,0 +1,235 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim 
+import torch.nn.functional as F
+import collections
+from collections import namedtuple
+from wimblepong import Wimblepong
+import random
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+warnings.filterwarnings("ignore", category=UserWarning)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+class DQN(nn.Module):
+    def __init__(self, state_space_dim, action_space_dim, hidden=512):
+        super(DQN, self).__init__()
+        self.conv1 = nn.Conv2d(2, 32, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+        convDim = self.getConvDim()
+        self.fc1 = nn.Linear(convDim, hidden) #256
+        self.fc2 = nn.Linear(hidden, action_space_dim)
+        self.init_weights()
+
+    def init_weights(self):
+        for m in self.modules():
+            if type(m) is torch.nn.Linear:
+                torch.nn.init.uniform_(m.weight, 0, 1e-1)
+                torch.nn.init.zeros_(m.bias)
+            elif type(m) is torch.nn.Conv2d:
+                torch.nn.init.xavier_normal(m.weight)    
+
+    def getConvDim(self):
+        testinput = torch.zeros(1, 2, 100, 100)
+        x = self.conv1(testinput)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        convOut = self.conv3(x)
+        F.relu(x)
+        return int(np.prod(convOut.size()))
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = x.view(x.size()[0], -1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        return x
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward', 'done'))
+
+class ReplayMemory(object):
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, *args):
+        """Saves a transition."""
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+        
+class Agent(object):
+    def __init__(self, env, state_space, player_id=1):
+        if type(env) is not Wimblepong:
+            raise TypeError("I'm not a very smart AI. All I can play is Wimblepong.")
+        self.env = env
+        self.reset()
+        # Set the player id that determines on which side the ai is going to play
+        self.player_id = player_id
+        self.train_device = "cuda" if torch.cuda.is_available() else "cpu"
+        #self.train_device = "cuda"
+        #self.train_device = "cpu"                  
+        self.name = "DQN.AI"
+        self.policy_net = DQN(state_space, 3).to(self.train_device) #hua
+        #self.target_net = DQN(self.env.observation_space.shape, 3).to(self.train_device)
+        self.target_net = DQN(state_space, 3).to(self.train_device) #hua
+        self.epsilon = 1.0  # may need to change
+        self.n_actions = 3 #maybe change to env size
+        self.optimizer = optim.Adam(self.policy_net.parameters(), lr=0.00025)
+        self.memory = ReplayMemory(100000) #org correct one
+        #self.memory = ReplayMemory(10000) #0612
+        #self.memory = ReplayMemory(5000) # 0512 testing
+        self.batch_size = 32 
+        self.gamma = 0.99
+        self.prev_obs = None  # hua
+        self.policy_net.eval()    #hua
+        self.state = None  # hua
+
+    def reset(self):
+        self.state = self.env.reset()
+
+    def get_name(self):
+        """
+        Interface function to retrieve the agents name
+        """
+        return self.name
+
+    def replace_targetpolicy(self): #hua
+        
+        self.target_net.load_state_dict(self.policy_net.state_dict()) #hua
+
+    def get_action(self, ob=None): # ob ?
+        """
+        Interface function that returns the action that the agent took based
+        on the observation ob
+        """
+        sample = random.random()
+        if sample >= self.epsilon:
+            with torch.no_grad():
+                #x = self.preprocess(ob).to(self.train_device)
+                #print(self.state.shape)
+                state = self.preprocess(self.state).to(self.train_device)   #hua "Here"
+                #state = torch.from_numpy(np.ascontiguousarray(self.state)).float() #hua
+                #state = state.view(1,self.state.shape[2], self.state.shape[0], self.state.shape[1]) #hua
+                #print("here, state shape:",state.shape)
+                q_values = self.policy_net.forward(state)
+                #print(q_values)
+                action = torch.argmax(q_values).item()
+                #print(action)
+                return action
+        else:
+            return random.randrange(self.n_actions)
+    '''
+    def store_transitionO(self, state, action, next_state, reward, done):
+        action = torch.Tensor([[action]]).long()
+        reward = torch.tensor([reward], dtype=torch.float32)
+        next_state = torch.from_numpy(np.ascontiguousarray(next_state)).float()
+        state = torch.from_numpy(np.ascontiguousarray(state)).float()
+        self.memory.push(state, action, next_state, reward, done)
+    '''        
+    def store_transition(self, state, action, next_state, reward, done): #hua !!!
+        action = torch.Tensor([[action]]).long()
+        reward = torch.tensor([reward], dtype=torch.float32)
+        #next_state = torch.from_numpy(next_state).float()
+        next_state = self.preprocess(next_state)
+        #state = torch.from_numpy(state).float()
+        state = self.preprocess(state)
+        self.memory.push(state, action, next_state, reward, done)
+
+    def calculate_loss(self, sample):  # working here now
+        batch = Transition(*zip(*sample))
+        states = torch.stack(batch.state).to(self.train_device)  #h? todevice?
+        actions = torch.cat(batch.action).to(self.train_device)
+        rewards = torch.cat(batch.reward).to(self.train_device)
+        done = torch.ByteTensor(batch.done)
+        next_states = torch.cat(batch.next_state).to(self.train_device)
+        states = states.squeeze(1)
+        #non_final_mask = 1-torch.tensor(batch.done, dtype=torch.uint8)# need to change to boolean?
+        '''
+        non_final_next_states = [s for nonfinal,s in zip(non_final_mask,
+                                     batch.next_state) if nonfinal > 0]
+        non_final_next_states = torch.stack(non_final_next_states)
+        non_final_next_states = non_final_next_states.view(non_final_next_states.shape[0], 2, 100, 100)
+        state_action_values = self.policy_net(states).gather(1, actions)
+        next_state_values = torch.zeros(self.batch_size)
+        next_state_values[done] = self.target_net(non_final_next_states).max(1)[0].detach()
+        '''
+        state_action_values = self.policy_net(states).gather(1, actions.long()).unsqueeze(-1)
+        next_state_values = self.target_net(next_states).max(1)[0]
+        next_state_values[done] = 0.0
+        next_state_values = next_state_values.detach()
+        expected_values = next_state_values * self.gamma + rewards
+        return nn.MSELoss()(state_action_values, expected_values)   
+    '''    
+    def calculate_lossN(self, sample):  # here now working on
+        batch = Transition(*zip(*sample))
+        states = torch.stack(batch.state)  #h? todevice?
+        actions = torch.cat(batch.action)
+        rewards = torch.cat(batch.reward)
+        non_final_mask = 1-torch.tensor(batch.done, dtype=torch.uint8)# need to change to boolean?
+        non_final_next_states = [s for nonfinal,s in zip(non_final_mask,
+                                     batch.next_state) if nonfinal > 0]
+        non_final_next_states = torch.stack(non_final_next_states)
+        states = states.view(32, 3, 200, 200)
+        non_final_next_states = non_final_next_states.view(non_final_next_states.shape[0], 3, 200, 200)
+        state_action_values = self.policy_net(states).gather(1, actions)
+        next_state_values = torch.zeros(self.batch_size)
+        next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1)[0].detach()
+        expected_values = next_state_values * self.gamma + rewards
+        #print(rewards, expected_values, state_action_values)
+        return nn.MSELoss()(state_action_values, expected_values)
+    '''
+
+    def update_network(self):
+        if len(self.memory) < self.batch_size:
+            return
+        self.optimizer.zero_grad()
+        sample = self.memory.sample(self.batch_size)
+        loss = self.calculate_loss(sample)
+        #if self.epsilon < 0.2:
+        #    print(loss)
+        loss.backward()
+        for param in self.policy_net.parameters():
+            param.grad.data.clamp_(-0.1, 0.1)
+        self.optimizer.step()
+
+    def save_model(self):
+        torch.save(self.policy_net.state_dict(), "modelhit.mdl")
+
+    def load_model(self, modelfile):
+        print("Loading model from file ", modelfile)
+        self.policy_net.load_state_dict(torch.load(modelfile, map_location=lambda storage, loc: storage))
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+        self.epsilon = 0.1
+        return
+
+    def preprocess(self, observation):
+        observation = observation[::2, ::2].mean(axis=-1)
+        observation = np.expand_dims(observation, axis=-1)
+        if self.prev_obs is None:
+            self.prev_obs = observation
+        stack_ob = np.concatenate((self.prev_obs, observation), axis=-1)
+        stack_ob = torch.from_numpy(stack_ob).float().unsqueeze(0)
+        stack_ob = stack_ob.transpose(1, 3)
+        self.prev_obs = observation
+        return stack_ob    
+ 
+
